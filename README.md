61ff269867a4351898a0db88ebf9feed4f1111a1
westy12dan <westy12dan@gmail.com>
2023-03-24 00:32:40 +0800

    feat(readme): Add readme and fix code

---
+++ README.md
@@ -0,0 +1,57 @@
+# fgit
+
+fgit is a command-line tool that wraps around Git and provides a simpler way to create conventional commits. Conventional commits follow a standardized format for commit messages, making it easier to understand the purpose of a change.
+
+## Background
+
+Conventional commits were created to improve communication and collaboration between developers working on a project. The format consists of a type, scope, and description, with optional footer information such as issue numbers or breaking changes. The standardized format helps to ensure that commit messages are clear and easy to understand, making it easier to track changes and troubleshoot issues.
+
+## Building the Binary
+
+To use fgit, you need to build the binary first. Here's how to do it:
+
+- Install Rust by following the instructions on the [official website](https://www.rust-lang.org/tools/install).
+- Clone this repo onto your computer
+- Build the binary by running `cargo build --release`. The binary will be placed in the target/release directory.
+
+### Assigning the Binary to an Executable
+
+- Ensure you have built the binary first using the steps above
+- To make the binary executable directly like "fgit", you need to add it to your system's PATH environment variable. Here are the steps for different platforms:
+
+#### Linux/WSL
+
+- Open a terminal window and navigate to your project directory.
+- Run the following command to add the binary to your PATH:
+
+```bash
+export PATH="$PATH:/path/to/my-project/target/release"
+# Replace /path/to/my-project with the path to your project directory.
+# You can get this information by running the command pwd from the project directory.
+```
+
+- To make this change permanent, you can add this command to your shell's configuration file (e.g. ~/.bashrc for Bash or ~/.zshrc for Zsh).
+- After completing these steps, you should be able to run your binary by typing fgit in a terminal window.
+
+## Usage
+
+Here are some examples of how to use fgit:
+
+```bash
+$ fgit commit feat user Add login form # git commit -m 'feat(user): Add login form'
+$ fgit commit chore deps 'Update to latest' # git commit -m 'chore(deps): Update to latest'
+```
+
+It also can pull out ticket numbers from branch information if your branch name is formatted like `issue/ABC-123` or `issue/ABC-1-extra-info`.
+
+```bash
+# current branch: issue/ABC-123
+$ fgit commit feat user Add login form # git commit -m 'feat(user): Add login form [ABC-123]'
+$ fgit commit chore deps 'Update to latest' # git commit -m 'chore(deps): Update to latest [ABC-123]'
+```
+
+If installed using the instructions above, you can also update fgit by running:
+
+```bash
+$ fgit update
+```
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
-use std::env;
-use std::process::{Command, exit};
+use std::process::{exit, Command};
+use std::{env, io};
 
-const VALID_TYPES: [&str; 10] = ["feat", "fix", "build", "chore", "ci", "docs", "style", "refactor", "perf", "test"];
+const VALID_TYPES: [&str; 10] = [
+    "feat", "fix", "build", "chore", "ci", "docs", "style", "refactor", "perf", "test",
+];
+const VERSION: &str = "1.0.0";
+
+const HELP_TEXT: &str = "Usage: fgit [command]
+
+Wrapper around git. All unhandled commands are handled by git.
+
+Commands:
+    commit <type> <scope> <description>
+    update
+    --help     Show help information
+    --version  Show version information";
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    let mut git_command = Command::new("git");
+    let mut git_command = Command::new("echo");
 
     // Handle --help and --version options
     if let Some(option) = args.iter().next() {
         match option.as_str() {
             "--help" => {
-                println!("Usage: fgit [command]\n\nWrapper around git. All unhandled commands are handled by git.\n\nCommands:\n  commit <type> <scope> <description>\n  --help     Show help information\n  --version  Show version information\n");
+                println!("{}", HELP_TEXT);
                 exit(0);
             }
             "--version" => {
-                println!("fgit version 1.0.0");
+                println!("fgit version {}", VERSION);
                 exit(0);
             }
             _ => {}
@@ -26,6 +39,40 @@ fn main() {
     if args.is_empty() {
         eprintln!("No git command provided");
         exit(1);
+    }
+
+    // Handle updating fgit
+    if args[0] == "update" {
+        let output = Command::new("git")
+            .arg("fetch")
+            .arg("--dry-run")
+            .arg("origin")
+            .arg("main")
+            .output()
+            .expect("Failed to execute git command");
+
+        if output.stdout.is_empty() {
+            println!("fgit is up to date");
+        } else {
+            println!("New version of fgit is available. Do you want to update? (y/n)");
+            let mut input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read input");
+
+            if input.trim().to_lowercase() == "y" {
+                let output = Command::new("git")
+                    .arg("pull")
+                    .arg("origin")
+                    .arg("main")
+                    .output()
+                    .expect("Failed to execute git command");
+                println!("{}", String::from_utf8_lossy(&output.stdout));
+                exit(0);
+            }
+        }
+
+        exit(0);
     }
 
     // Handle git commit command
@@ -52,12 +99,17 @@ fn main() {
             .arg("HEAD")
             .output()
             .expect("Failed to execute git command");
-        let branch_name = String::from_utf8_lossy(&branch_output.stdout).trim().to_string();
+        let branch_name = String::from_utf8_lossy(&branch_output.stdout)
+            .trim()
+            .to_string();
 
         if branch_name.starts_with("issue/") {
             if let Some(issue_number) = branch_name[6..].find('-') {
                 let issue_key = &branch_name[6..6 + issue_number];
-                let issue_number = &branch_name[6 + issue_number + 1..].chars().take_while(|c| c.is_ascii_digit()).collect::<String>();
+                let issue_number = &branch_name[6 + issue_number + 1..]
+                    .chars()
+                    .take_while(|c| c.is_ascii_digit())
+                    .collect::<String>();
                 commit_message.push_str(&format!(" [{}-{}]", issue_key, issue_number));
             }
         }
