aafea05fc06120237db3174ed6274462f7064749
westy12dan <westy12dan@gmail.com>
2023-03-23 23:39:45 +0800

    fix(ci): Add comments, help, and version

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,46 @@ const VALID_TYPES: [&str; 10] = ["feat", "fix", "build", "chore", "ci", "docs", 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
     let mut git_command = Command::new("git");
-    
+
+    // Handle --help and --version options
+    if let Some(option) = args.iter().next() {
+        match option.as_str() {
+            "--help" => {
+                println!("Usage: fgit [command]\n\nWrapper around git. All unhandled commands are handled by git.\n\nCommands:\n  commit <type> <scope> <description>\n  --help     Show help information\n  --version  Show version information\n");
+                exit(0);
+            }
+            "--version" => {
+                println!("fgit version 1.0.0");
+                exit(0);
+            }
+            _ => {}
+        }
+    }
+
+    // Check if git command is provided
     if args.is_empty() {
         eprintln!("No git command provided");
         exit(1);
     }
-    
+
+    // Handle git commit command
     if args[0] == "commit" {
         if args.len() < 3 {
             eprintln!("Usage: fgit commit <type> <scope> <description>");
             exit(1);
         }
-        
+
         let commit_type = args[1].to_lowercase();
         if !VALID_TYPES.contains(&&*commit_type) {
             eprintln!("Invalid commit type '{}'", commit_type);
             exit(1);
         }
-        
+
         let scope = args[2].to_lowercase();
         let description = args[3..].join(" ");
         let mut commit_message = format!("{}({}): {}", commit_type, scope, description);
 
+        // Append issue number if current branch name starts with "issue/XXX-000"
         let branch_output = Command::new("git")
             .arg("rev-parse")
             .arg("--abbrev-ref")
@@ -43,12 +61,12 @@ fn main() {
                 commit_message.push_str(&format!(" [{}-{}]", issue_key, issue_number));
             }
         }
-        
+
         git_command.arg("commit").arg("-m").arg(commit_message);
     } else {
         git_command.args(&args);
     }
-    
+
     let status = git_command.status().expect("Failed to execute git command");
     exit(status.code().unwrap_or(1));
 }
